ad1964a1d7df8df53da373d0d36b8d983eff5d91
kimaster <kimaster@live.ca>
2022-11-26 18:04:56 -0500

    extracting star and number component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
-function StarMatch(){
 
+
+function StarMatch(){
+  const stars=utils.random(1,9);
    return(<div className='game'>
       <div className='help"'>
         Please pick 1 or more numbers that sum the number of stars
       </div>
       <div className='game-board'>
         <div className='stars-container'>
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
-          <div className="star" />
+        {
+          utils.range(1,stars).map(starId=>
+            <div key={starId} className='star'></div>)
+
+        }  
         </div>
         <div className='nums-container'>
-          <button className="number">1</button>
-          <button className="number">2</button>
-          <button className="number">3</button>
-          <button className="number">4</button>
-          <button className="number">5</button>
-          <button className="number">6</button>
-          <button className="number">7</button>
-          <button className="number">8</button>
-          <button className="number">9</button>
+
+        {
+          utils.range(1,9).map(Number=>
+            <button className="number">{Number}</button>)
+
+        }  
+ 
         </div>
       </div>
       <div className="timer">Time Remaining: 10</div>
@@ -40,6 +36,38 @@ available:'gray',
 used:'lightgreen',
 wrong:'lightred',
 ccandidate:'brown'
+
+}
+
+const utils = {
+  // Sum an array
+  sum: arr => arr.reduce((acc, curr) => acc + curr, 0),
+
+  // create an array of numbers between min and max (edges included)
+  range: (min, max) => Array.from({ length: max - min + 1 }, (_, i) => min + i),
+
+  // pick a random number between min and max (edges included)
+  random: (min, max) => min + Math.floor(Math.random() * (max - min + 1)),
+
+  // Given an array of numbers and a max...
+  // Pick a random sum (< max) from the set of all available sums in arr
+  randomSumIn: (arr, max) => {
+    const sets = [[]];
+    const sums = [];
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0, len = sets.length; j < len; j++) {
+        const candidateSet = sets[j].concat(arr[i]);
+        const candidateSum = utils.sum(candidateSet);
+        if (candidateSum <= max) {
+          sets.push(candidateSet);
+          sums.push(candidateSum);
+        }
+      }
+    }
+    return sums[utils.random(0, sums.length - 1)];
+  },
+};
+const helper={
 
 }
 function App() {
