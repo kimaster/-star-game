0a0c0ba753e64cadfb1531b463fe19c1d7734871
kimaster <kimaster@live.ca>
2022-11-26 17:33:23 -0500

    adding colors

--- src/App.js
+++ src/App.js
@@ -9,13 +9,38 @@ function StarMatch(){
       </div>
       <div className='game-board'>
         <div className='stars-container'>
-          stars
+          <div className="star" />
+          <div className="star" />
+          <div className="star" />
+          <div className="star" />
+          <div className="star" />
+          <div className="star" />
+          <div className="star" />
+          <div className="star" />
+          <div className="star" />
         </div>
         <div className='nums-container'>
-           numbers
+          <button className="number">1</button>
+          <button className="number">2</button>
+          <button className="number">3</button>
+          <button className="number">4</button>
+          <button className="number">5</button>
+          <button className="number">6</button>
+          <button className="number">7</button>
+          <button className="number">8</button>
+          <button className="number">9</button>
         </div>
       </div>
+      <div className="timer">Time Remaining: 10</div>
     </div>)
+}
+
+const colors={
+available:'gray',
+used:'lightgreen',
+wrong:'lightred',
+ccandidate:'brown'
+
 }
 function App() {
   return (
